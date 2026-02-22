eab987b8d6e15f5c36ce944eaf0bbd1b5a33d168
catheisabelle <cathy070606@gmail.com>
2026-02-22 20:25:17 +0700

    first commit

---
+++ Downloads/nextjs-firebase-catherine-main 2/components/LoginForm.js
@@ -0,0 +1,29 @@
+'use client'
+import { useState } from 'react'
+
+export default function LoginForm() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Login attempted')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  )
+}
