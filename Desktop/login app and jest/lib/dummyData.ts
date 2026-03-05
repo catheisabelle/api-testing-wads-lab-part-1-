947da86e36ac35dbbbf29f28bfa2fdeb1fd2acab
catheisabelle <cathy070606@gmail.com>
2026-03-05 16:02:38 +0700

    Delete Desktop directory

--- Desktop/login app and jest/lib/dummyData.ts
+++
@@ -1,5 +0,0 @@
-export let users = [
-    { id: 1, name: "Catherine", email: "catherine@email.com" },
-    { id: 2, name: "Avery", email: "avery@email.com" },
-    { id: 3, name: "Oscar", email: "oscar@email.com" }
-];
